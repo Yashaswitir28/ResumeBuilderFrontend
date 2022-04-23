77818963d324c5141d8bdeaad7e31a421b9403a8
Akash Tureha <mrmischievousx@DeadDude.local>
2022-04-23 09:48:01 +0530

    Updates

---
+++ precache-manifest.a4753201ecb5543287b9d8659f30947e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a487d8acd3b793bedd516e8a08594a45",
+    "url": "/resumebuilderfrontend/index.html"
+  },
+  {
+    "revision": "d76be0ec89892e7614fe",
+    "url": "/resumebuilderfrontend/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "22b12298d00c8df6a0fb",
+    "url": "/resumebuilderfrontend/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "d76be0ec89892e7614fe",
+    "url": "/resumebuilderfrontend/static/js/2.aa4553aa.chunk.js"
+  },
+  {
+    "revision": "cf422afd9ef9f78ea20aca31ed637782",
+    "url": "/resumebuilderfrontend/static/js/2.aa4553aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22b12298d00c8df6a0fb",
+    "url": "/resumebuilderfrontend/static/js/main.b537a7b8.chunk.js"
+  },
+  {
+    "revision": "0a7eb420a8814090f695",
+    "url": "/resumebuilderfrontend/static/js/runtime-main.3514bc93.js"
+  }
+]);
